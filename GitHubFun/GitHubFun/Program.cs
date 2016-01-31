712f9384cec4f5d825ac94105ee0c3a0fa85da60
jb <belford.james@outook.com>
2016-01-30 20:29:22 -0800

    Testing Branch1 commit

--- GitHubFun/GitHubFun/Program.cs
+++ GitHubFun/GitHubFun/Program.cs
@@ -11,6 +11,7 @@ namespace GitHubFun
         static void Main(string[] args)
         {
             Console.WriteLine("Hello GitHub"); // A comment on gitHub to Pull1
+            Console.WriteLine("TestBranch1");
         }
     }
 }
